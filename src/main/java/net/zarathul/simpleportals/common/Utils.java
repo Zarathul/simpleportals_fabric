9681fbcb3f0a720579473838a4658eab97afe375
Zarathul <Zarathul@users.noreply.github.com>
2020-10-27 16:00:02 +0100

    Removed unused imports.

--- src/main/java/net/zarathul/simpleportals/common/Utils.java
+++ src/main/java/net/zarathul/simpleportals/common/Utils.java
@@ -17,7 +17,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.portal.PortalInfo;
 import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.phys.Vec3;
 import net.zarathul.simpleportals.Settings;
