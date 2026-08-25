0e13624d5c8a4d6ed2da2ff43737f452ab827db7
Zarathul <Zarathul@users.noreply.github.com>
2026-08-26 00:44:20 +0200

    Item tooltips are now handled by Itemregistrar.

--- src/main/java/net/zarathul/simpleportals/ClientInit.java
+++ src/main/java/net/zarathul/simpleportals/ClientInit.java
@@ -1,25 +1,13 @@
 package net.zarathul.simpleportals;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
-import net.zarathul.simplemodslib.Utils;
 import net.zarathul.simplemodslib.api.configuration.Config;
 import net.zarathul.simpleportals.items.ModItems;
 import net.zarathul.simpleportals.network.ClientPacketHandlers;
-import org.lwjgl.glfw.GLFW;
 
 public class ClientInit implements ClientModInitializer
 {
-	private static final String PORTAL_ACTIVATOR_TOOLTIP_KEY         = "item." + SimplePortals.MOD_ID + "." + ModItems.PORTAL_ACTIVATOR_NAME + ".tooltip";
-	private static final String PORTAL_ACTIVATOR_TOOLTIP_DETAILS_KEY = "item." + SimplePortals.MOD_ID + "." + ModItems.PORTAL_ACTIVATOR_NAME + ".tooltip_details";
-	private static final String FRAME_TOOLTIP_KEY         = "item." + SimplePortals.MOD_ID + "." + ModItems.FRAME_NAME + ".tooltip";
-	private static final String FRAME_TOOLTIP_DETAILS_KEY = "item." + SimplePortals.MOD_ID + "." + ModItems.FRAME_NAME + ".tooltip_details";
-	private static final String POWER_GAUGE_TOOLTIP_KEY         = "item." + SimplePortals.MOD_ID + "." + ModItems.POWER_GAUGE_NAME + ".tooltip";
-	private static final String POWER_GAUGE_TOOLTIP_DETAILS_KEY = "item." + SimplePortals.MOD_ID + "." + ModItems.POWER_GAUGE_NAME + ".tooltip_details";
-
 	@Override
 	public void onInitializeClient()
 	{
@@ -35,45 +23,6 @@ public class ClientInit implements ClientModInitializer
 		// Custom Packets
 		ClientPacketHandlers.register();
 
-		// Tooltips
-		ItemTooltipCallback.EVENT.register((stack, tooltipContext, tooltipFlag, lines) -> {
-			String TOOLTIP_KEY;
-			String TOOLTIP_DETAILS_KEY;
-
-			if (stack.getItem() == ModItems.FRAME)
-			{
-				TOOLTIP_KEY = FRAME_TOOLTIP_KEY;
-				TOOLTIP_DETAILS_KEY = FRAME_TOOLTIP_DETAILS_KEY;
-			}
-			else if (stack.getItem() == ModItems.POWER_GAUGE)
-			{
-				TOOLTIP_KEY = POWER_GAUGE_TOOLTIP_KEY;
-				TOOLTIP_DETAILS_KEY = POWER_GAUGE_TOOLTIP_DETAILS_KEY;
-			}
-			else if (stack.getItem() == ModItems.PORTAL_ACTIVATOR)
-			{
-				TOOLTIP_KEY = PORTAL_ACTIVATOR_TOOLTIP_KEY;
-				TOOLTIP_DETAILS_KEY = PORTAL_ACTIVATOR_TOOLTIP_DETAILS_KEY;
-			}
-			else
-			{
-				return;
-			}
-
-			var mc = Minecraft.getInstance();
-			long windowHandle = mc.getWindow().handle();
-			int leftShiftState = GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_LEFT_SHIFT);
-			int rightShiftState = GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT);
-
-			if (leftShiftState == GLFW.GLFW_PRESS || rightShiftState == GLFW.GLFW_PRESS)
-			{
-				int maxWidth = mc.getWindow().getGuiScaledWidth() / 3;
-				lines.addAll(Utils.multiLineTranslateWithMaxWidth(TOOLTIP_DETAILS_KEY, maxWidth));
-			}
-			else
-			{
-				lines.add(Component.translatable(TOOLTIP_KEY));
-			}
-		});
+		ModItems.registerTooltips();
 	}
 }
--- src/main/java/net/zarathul/simpleportals/items/ModItems.java
+++ src/main/java/net/zarathul/simpleportals/items/ModItems.java
@@ -13,13 +13,9 @@ public final class ModItems
 {
 	private static final ItemRegistrar REGISTRAR = new ItemRegistrar(SimplePortals.MOD_ID);
 
-	public static final String FRAME_NAME = "portal_frame";
-	public static final String POWER_GAUGE_NAME = "power_gauge";
-	public static final String PORTAL_ACTIVATOR_NAME = "portal_activator";
-
-	public static final BlockItem FRAME = REGISTRAR.register(FRAME_NAME, ModBlocks.FRAME, BlockItem::new, new Item.Properties().stacksTo(64));
-	public static final BlockItem POWER_GAUGE = REGISTRAR.register(POWER_GAUGE_NAME, ModBlocks.POWER_GAUGE, BlockItem::new, new Item.Properties().stacksTo(64));
-	public static final ItemPortalActivator PORTAL_ACTIVATOR = REGISTRAR.register(PORTAL_ACTIVATOR_NAME, ItemPortalActivator::new, new Item.Properties().stacksTo(1));
+	public static final BlockItem FRAME = REGISTRAR.register("portal_frame", ModBlocks.FRAME, BlockItem::new, new Item.Properties().stacksTo(64));
+	public static final BlockItem POWER_GAUGE = REGISTRAR.register("power_gauge", ModBlocks.POWER_GAUGE, BlockItem::new, new Item.Properties().stacksTo(64));
+	public static final ItemPortalActivator PORTAL_ACTIVATOR = REGISTRAR.register("portal_activator", ItemPortalActivator::new, new Item.Properties().stacksTo(1));
 
 	public static void init()
 	{
@@ -30,5 +26,10 @@ public final class ModItems
 			POWER_GAUGE,
 			PORTAL_ACTIVATOR
 		);
+	}
+
+	public static void registerTooltips()
+	{
+		REGISTRAR.registerTooltips();
 	}
 }
